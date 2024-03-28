8970c04a61b042422c846f2afdb7e24d7e675bd2
Autumn Na <autumn.na.dev@gmail.com>
2024-03-28 21:36:58 +0900

    CSS-34 [프로그램][신규] 스테이지_트리거_추상화 클리어와 게임 오버를 판단하는 Trigger 클래스와 관련 로직 구현

---
+++ capstone_2024_20/Source/capstone_2024_20/Trigger/Trigger.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Trigger.generated.h"
+
+class ASailingSystem;
+
+UCLASS()
+class CAPSTONE_2024_20_API UTrigger : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UTrigger();
+	void Initialize(const FString& Id, ASailingSystem* InSailingSystem);
+
+	bool (*IsTriggered)();
+
+	UPROPERTY()
+	ASailingSystem* SailingSystem;
+};
