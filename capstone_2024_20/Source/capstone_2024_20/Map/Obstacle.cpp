ace912d7d5b07601e3a8b464842e753664751e55
Autumn Na <autumn.na.dev@gmail.com>
2024-04-12 06:56:08 +0900

    CSS-123 [프로그램][신규] 항해_맵_생성 알고리즘 맵에 생성할 장애물 Actor 추가 - 향후 Blueprint로 변경

---
+++ capstone_2024_20/Source/capstone_2024_20/Map/Obstacle.cpp
@@ -0,0 +1,13 @@
+﻿#include "Obstacle.h"
+
+AObstacle::AObstacle() : MeshComponent(nullptr)
+{
+	// TODO@autumn - This is a temporary mesh, replace it with the actual mesh from data
+	
+	const auto Random = FMath::RandRange(1, 3);
+	const auto Name = FString::Printf(TEXT("/Game/GameObjects/Terrain/Rock%d.Rock%d"), Random, Random);
+	
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	MeshComponent->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, *Name));
+	RootComponent = MeshComponent;
+}
