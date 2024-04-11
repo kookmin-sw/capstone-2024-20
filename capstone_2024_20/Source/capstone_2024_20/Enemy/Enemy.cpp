8d7b5785fe0ebd454f1e64ae4a04f0e918bf0c63
Autumn Na <autumn.na.dev@gmail.com>
2024-04-12 02:46:11 +0900

    CSS-124 [프로그램][신규] 항해_적_해적을 향해 이동 부자연스러운 적 생성 알고리즘 일부 수정

--- capstone_2024_20/Source/capstone_2024_20/Enemy/Enemy.cpp
+++ capstone_2024_20/Source/capstone_2024_20/Enemy/Enemy.cpp
@@ -4,7 +4,7 @@ AEnemy::AEnemy(): StaticMesh(nullptr)
 {
 	// TODO@autumn - This is a temporary mesh, replace it with the actual mesh from data
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
-	StaticMesh->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, TEXT("/Script/Engine.StaticMesh'/Game/GameObjects/CannonBall/CannonBall_mesh.CannonBall_mesh'")));
+	StaticMesh->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, TEXT("/Script/Engine.StaticMesh'/Game/GameObjects/TreasureBox/TreasureBox_Box.TreasureBox_Box'")));
 	RootComponent = StaticMesh;
 }
 
