43f29167bdaef37b46fe76fcdcfec5a3256eab5a
Autumn Na <autumn.na.dev@gmail.com>
2024-04-19 05:30:00 +0900

    CSS-168 [프로그램][신규] 항해_배_업그레이드 업그레이드 오브젝트 구현 및 배치

---
+++ capstone_2024_20/Source/capstone_2024_20/Object/UpgradeObject.cpp
@@ -0,0 +1,41 @@
+﻿#include "UpgradeObject.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/BoxComponent.h"
+
+AUpgradeObject::AUpgradeObject(): SkeletalMesh(nullptr), BoxComponent(nullptr)
+{
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMesh->SetSkeletalMesh(LoadObject<USkeletalMesh>(nullptr, TEXT("/Script/Engine.SkeletalMesh'/Game/GameObjects/UpgradeNpc/UpgradeNpc.UpgradeNpc'")));
+	RootComponent = SkeletalMesh;
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(RootComponent);
+	BoxComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	BoxComponent->SetBoxExtent(FVector(150.0f, 150.0f, 100.0f));
+	BoxComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	BoxComponent->SetGenerateOverlapEvents(true);
+
+	CanBeOperated = true;
+}
+
+void AUpgradeObject::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AUpgradeObject::Operate()
+{
+	Super::Operate();
+
+	const auto PopupUpgradeRef = TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/WidgetBlueprints/UpgradeObject/BP_PopupUpgrade.BP_PopupUpgrade_C'");
+	if (const auto PopupUpgradeClass = StaticLoadClass(UUserWidget::StaticClass(), nullptr,PopupUpgradeRef); PopupUpgradeClass != nullptr)
+	{
+		if (UUserWidget* PopupUpgrade = CreateWidget<UUserWidget>(GetWorld(), PopupUpgradeClass); PopupUpgrade != nullptr)
+		{
+			PopupUpgrade->AddToViewport();
+
+			// enable mouse cursor
+			GetWorld()->GetFirstPlayerController()->bShowMouseCursor = true;
+		}
+	}	
+}
