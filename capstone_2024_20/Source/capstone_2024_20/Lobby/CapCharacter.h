979233c474f30d76fcf59a0706d23a339e3b6c19
hyunseo <hansx2079@naver.com>
2024-04-19 21:23:17 +0900

    [CSS-148] Lobby Characeter Class 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/Lobby/CapCharacter.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CapCharacter.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+class UCameraComponent;
+class USpringArmComponent;
+struct FInputActionValue;
+
+UCLASS()
+class CAPSTONE_2024_20_API ACapCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere)
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere)
+	UInputAction* MoveAction;
+
+public:
+	ACapCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& Value);
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+	void InitSpringArmCamera();
+	void InitMovement();
+};
