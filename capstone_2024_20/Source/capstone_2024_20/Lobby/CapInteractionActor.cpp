0367f1651548612dc69f2cffb2b09099fd03ae9c
hyunseo <hansx2079@naver.com>
2024-05-03 01:05:00 +0900

    [CSS-205] Lobby Long Interaction 작용

---
+++ capstone_2024_20/Source/capstone_2024_20/Lobby/CapInteractionActor.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CapInteractionActor.h"
+
+
+ACapInteractionActor::ACapInteractionActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ACapInteractionActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACapInteractionActor::InteractionEnter()
+{
+	Super::InteractionEnter();
+	GetWorldTimerManager().SetTimer(EnterTimerHandle, this, ThisClass::InteractionLongEnter, LongInteractionThreshold);
+}
+
+void ACapInteractionActor::InteractionExit()
+{
+	GetWorldTimerManager().ClearTimer(EnterTimerHandle);
+	Super::InteractionExit();
+}
+
+void ACapInteractionActor::InteractionLongEnter()
+{
+}
