cd6ef3bc3ff97b5e606bddf6aca52df8ea7a424f
Autumn Na <autumn.na.dev@gmail.com>
2024-05-11 22:15:21 +0900

    CSS-221 [프로그램][개선] 항해_적_Character 기반으로 수정 부모 클래스가 Character인 Replicated 클래스 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/01_Network/ReplicatedCharacter.cpp
@@ -0,0 +1,54 @@
+﻿#include "ReplicatedCharacter.h"
+#include "Net/UnrealNetwork.h"
+
+AReplicatedCharacter::AReplicatedCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(RootComponent);
+}
+
+void AReplicatedCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AReplicatedCharacter, Location);
+	DOREPLIFETIME(AReplicatedCharacter, Rotation);
+}
+
+void AReplicatedCharacter::OnRep_Location()
+{
+	if (HasAuthority())
+	{
+		Location = GetActorLocation();
+	}
+	else
+	{
+		SetActorLocation(Location);
+	}
+}
+
+void AReplicatedCharacter::OnRep_Rotation()
+{
+	if (HasAuthority())
+	{
+		Rotation = GetActorRotation();
+	}
+	else
+	{
+		SetActorRotation(Rotation);
+	}
+}
+
+void AReplicatedCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AReplicatedCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ capstone_2024_20/Source/capstone_2024_20/01_Network/ReplicatedCharacter.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ReplicatedCharacter.generated.h"
+
+UCLASS()
+class CAPSTONE_2024_20_API AReplicatedCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AReplicatedCharacter();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(Replicated = OnRep_Location)
+	FVector Location;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Rotation)
+	FRotator Rotation;
+
+	UFUNCTION()
+	void OnRep_Location();
+	UFUNCTION()
+	void OnRep_Rotation();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
