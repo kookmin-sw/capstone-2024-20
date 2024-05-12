976d620dd048573eee63a2c9e42852d7683a29ed
hyunseo <hansx2079@naver.com>
2024-05-13 02:01:25 +0900

    [CSS-231] CharacterTypeFactory 생성

---
+++ capstone_2024_20/Source/capstone_2024_20/Common/CharacterTypeFactory.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CharacterTypeFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_2024_20_API UCharacterTypeFactory : public UObject
+{
+	GENERATED_BODY()
+};
