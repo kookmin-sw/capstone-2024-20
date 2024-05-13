47189532dc127b5249d5d6038fdfb154d367d50c
hyunseo <hansx2079@naver.com>
2024-05-13 15:47:12 +0900

    [CSS-231] CharacterTypePath

---
+++ capstone_2024_20/Source/capstone_2024_20/CharacterType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+UENUM()
+enum ECharacterType
+{
+	Character1,
+	Character2,
+	Character3,
+};
+
+namespace FCharacterTypePath
+{
+	const FString Character1 = TEXT("/Game/Blueprints/Character/BP_Character1.BP_Character1'");
+	const FString Character2 = TEXT("/Game/Blueprints/Character/BP_Character1.BP_Character1'");
+}
