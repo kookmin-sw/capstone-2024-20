3bbbdc41da9184470bf2e6a476d152f584791460
Autumn Na <autumn.na.dev@gmail.com>
2024-05-13 21:54:44 +0900

    CSS-228 [프로그램][신규] 항해_해적_사망 의미 없는 공백 제거, 컨벤션에 맞지 않는 내용 수정

--- capstone_2024_20/Source/capstone_2024_20/CharacterControlStrategy.cpp
+++ capstone_2024_20/Source/capstone_2024_20/CharacterControlStrategy.cpp
@@ -1,17 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CharacterControlStrategy.h"
-
 #include "MyCharacter.h"
 
 void CharacterControlStrategy::Move(const FInputActionInstance& Instance, AActor* Actor,APlayerController* PlayerController, float DeltaTime)
 {
-	AMyCharacter* Character = Cast<AMyCharacter>(Actor);
-	if (Character)
+	if (AMyCharacter* Character = Cast<AMyCharacter>(Actor))
 	{
-		FVector2D MoveVec = Instance.GetValue().Get<FVector2D>();
-
+		const FVector2D MoveVec = Instance.GetValue().Get<FVector2D>();
 		const FRotator Rotation = Character->GetControlRotation();
 		const FRotator YawRotation(0,Rotation.Yaw,0);
 		const FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
