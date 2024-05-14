3a149ec68ae168fc0d74136b00a8efc12d7876ad
Autumn Na <autumn.na.dev@gmail.com>
2024-05-14 21:29:44 +0900

    CSS-243 [프로그램][신규] 항해_해적_공격 애니메이션 해적 액터의 AnimInstance 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/Pirate/PirateAnimInstance.cpp
@@ -0,0 +1,14 @@
+﻿#include "PirateAnimInstance.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UPirateAnimInstance::OnAttackEnd()
+{
+	bIsAttacking = false;
+}
+
+void UPirateAnimInstance::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UPirateAnimInstance, bIsAttacking);
+}
