afcc7e7e9c46e8b24d6f59e53e2aa6d2294ad735
Autumn Na <autumn.na.dev@gmail.com>
2024-05-15 03:04:03 +0900

    CSS-235 [프로그램][신규] 항해_해적_사망 UI 사망 팝업의 '남은 부활 시간'을 갱신하는 함수 추가

--- capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupDead.cpp
+++ capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupDead.cpp
@@ -1,1 +1,10 @@
 ﻿#include "PopupDead.h"
+
+#include "capstone_2024_20/MyCharacter.h"
+#include "Components/TextBlock.h"
+
+void UPopupDead::SetTextByReviveCooldown(const float ReviveCooldown) const
+{
+	UTextBlock* TextBlock = Cast<UTextBlock>(GetWidgetFromName(TEXT("TXT_Info")));
+	TextBlock->SetText(FText::FromString(FString::Printf(TEXT("사망하였습니다.\n%d초 뒤 부활합니다."), FMath::CeilToInt(ReviveCooldown))));
+}
--- capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupDead.h
+++ capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupDead.h
@@ -4,8 +4,14 @@
 #include "Blueprint/UserWidget.h"
 #include "PopupDead.generated.h"
 
+class AMyCharacter;
+
 UCLASS()
 class CAPSTONE_2024_20_API UPopupDead : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void SetTextByReviveCooldown(float ReviveCooldown) const;
+
 };
