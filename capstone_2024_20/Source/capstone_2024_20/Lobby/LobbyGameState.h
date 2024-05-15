c6b2ad3e58a193a56d9d7fcc40c670e71a70be26
hyunseo <hansx2079@naver.com>
2024-05-15 21:42:11 +0900

    [CSS-269] LobbyGameState

---
+++ capstone_2024_20/Source/capstone_2024_20/Lobby/LobbyGameState.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "capstone_2024_20/Common/CapGameState.h"
+#include "LobbyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_2024_20_API ALobbyGameState : public ACapGameState
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category="Network")
+	class ANetworkService* NetworkService;
+
+	UFUNCTION()
+	FName GetRoomCode() const;
+
+	UFUNCTION()
+	FName GetRoomName() const;
+};
