bbb61c5772cbd08a04fcf4e85b54a0c16bd7b1fb
hyunseo <hansx2079@naver.com>
2024-05-15 13:54:54 +0900

    [CSS-254] 업그레이드 UI 위젯 클래스 생성만

---
+++ capstone_2024_20/Source/capstone_2024_20/Upgrade/UpgradeWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UpgradeWidget.generated.h"
+
+UCLASS()
+class CAPSTONE_2024_20_API UUpgradeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
