f8c00ef0038d89409c7b44e7903234e6816fc9b5
Autumn Na <autumn.na.dev@gmail.com>
2024-05-17 00:48:11 +0900

    CSS-257 [프로그램][개선] 항해_적_생성 위치 변경 적 생성 위치를 표시하기 위한 SceneComponent 하위 클래스 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/Enemy/EnemySpawnPoint.cpp
@@ -0,0 +1,18 @@
+﻿#include "EnemySpawnPoint.h"
+
+UEnemySpawnPoint::UEnemySpawnPoint()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UEnemySpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UEnemySpawnPoint::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ capstone_2024_20/Source/capstone_2024_20/Enemy/EnemySpawnPoint.h
@@ -0,0 +1,25 @@
+﻿// Class for the enemy spawn point, basically do nothing but to be a reference point for the enemy to spawn
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "EnemySpawnPoint.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class CAPSTONE_2024_20_API UEnemySpawnPoint : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemySpawnPoint();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
