382c98d98b7c344892066f41c5697ab0db2e1148
Autumn Na <autumn.na.dev@gmail.com>
2024-05-19 00:24:25 +0900

    CSS-278 [프로그램][개선] 항해_해적_상호작용_UI UI 디자인 수정, 텍스쳐 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupInteraction.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PopupInteraction.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class CAPSTONE_2024_20_API UPopupInteraction : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* InteractionText;
+
+	void SetInteractionText(const FString& Text) const;
+};
