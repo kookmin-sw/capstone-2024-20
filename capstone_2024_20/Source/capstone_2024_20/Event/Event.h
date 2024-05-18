1c61e9d8b3b60ba18205430b217df2d163f6679d
Autumn Na <autumn.na.dev@gmail.com>
2024-05-18 21:33:15 +0900

    CSS-263 [프로그램][신규] 항해_이벤트_UI 이벤트 쿨다운이 지남에 따라, ProgressBar를 갱신하도록 작성 UI 갱신은 NetMulticast로 작성한다.

--- capstone_2024_20/Source/capstone_2024_20/Event/Event.h
+++ capstone_2024_20/Source/capstone_2024_20/Event/Event.h
@@ -6,6 +6,8 @@
 #include "Components/BoxComponent.h"
 #include "Event.generated.h"
 
+class UWidgetComponent;
+class UPopupEvent;
 class AMyShip;
 class AEvent;
 
@@ -21,9 +23,14 @@ public:
 
 	virtual void BeginPlay() override;
 	virtual void Operate() override;
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 	void DamageMyShip(AMyShip* MyShip);
 	void ReduceCurrentDamageCooldown(const float DeltaTime);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastRPC_ReduceCurrentDamageCooldown();
 	
 	bool CanDamageMyShip() const;
 	
@@ -36,9 +43,17 @@ private:
 	UPROPERTY(EditAnywhere)
 	UBoxComponent* BoxComponent;
 
+	UPROPERTY()
+	UWidgetComponent* PopupEventWidgetComponent;
+	
+	UPROPERTY()
+	UPopupEvent* PopupEvent;
+	
 	FVector AdjustedScale = FVector(0.4f, 0.4f, 0.4f);
 
 	const int Damage = 1;
 	const float DamageCooldown = 10.0f;
+
+	UPROPERTY(Replicated)
 	float CurrentDamageCooldown = 10.0f;
 };
