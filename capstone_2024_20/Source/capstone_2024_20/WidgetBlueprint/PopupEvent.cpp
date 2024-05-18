b01f0ac3ad7d2a6e757db6eedb2dcb916af6f54f
Autumn Na <autumn.na.dev@gmail.com>
2024-05-18 21:23:59 +0900

    CSS-263 [프로그램][신규] 항해_이벤트_UI ProgressBar를 가진 Widget 클래스 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupEvent.cpp
@@ -0,0 +1,7 @@
+﻿#include "PopupEvent.h"
+#include "Components/ProgressBar.h"
+
+void UPopupEvent::SetDamageCoolDownProgressBarPercent(const float Percent) const
+{
+	ProgressBar->SetPercent(Percent);
+}
