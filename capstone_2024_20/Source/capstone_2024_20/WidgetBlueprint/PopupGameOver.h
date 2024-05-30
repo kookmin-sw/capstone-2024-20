34673cf15191cb8b25f3fe057c0e05b1203d0fcc
Autumn Na <autumn.na.dev@gmail.com>
2024-05-30 23:53:41 +0900

    CSS-274 [프로그램][신규] 항해_클라이언트의 게임 오버 및 게임 클리어 팝업 게임 오버, 클리어 팝업의 클래스 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/WidgetBlueprint/PopupGameOver.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PopupGameOver.generated.h"
+
+class UButton;
+class UTextBlock;
+
+UCLASS()
+class CAPSTONE_2024_20_API UPopupGameOver : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* TextBlockWaitingServer;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* Button_0;
+
+	void Initialize(bool bHasAuthority) const;
+};
